5db122d0af72c219f8b34ef26e8a6b3b7e71e781
Tolik <tolikdolinskiy@gmail.com>
2021-07-14 13:25:38 +0300

    Updates

---
+++ precache-manifest.29f4a179dab173374337fa187191c9f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f0dd48bb97ebdbcce5cbed1bc7026b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a48591fb4cdc130542c",
+    "url": "./static/css/2.a1df12c0.chunk.css"
+  },
+  {
+    "revision": "67105f6f07af6263a636",
+    "url": "./static/css/main.bada76a3.chunk.css"
+  },
+  {
+    "revision": "5a48591fb4cdc130542c",
+    "url": "./static/js/2.31a53faa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.31a53faa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67105f6f07af6263a636",
+    "url": "./static/js/main.f0f86403.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
